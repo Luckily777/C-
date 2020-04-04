58e0097530f63d72a65b3c0d4025dc71f6d119b2
luckily777 <253974227@qq.com>
2020-04-04 20:55:59 +0800

    面试题02.03删除中间节点

---
+++ Likou040405.cpp
@@ -0,0 +1,17 @@
+面试题 02.03.删除中间节点
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
