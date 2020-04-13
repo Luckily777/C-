d48b72b0760bc9542583d06282504284c13e7f22
luckily777 <253974227@qq.com>
2020-04-13 17:49:35 +0800

    Linux的一些基础操作

---
+++ Linux041302.cpp
@@ -0,0 +1,46 @@
+[root@localhost 桌面]# vim hello.c
+[root@localhost 桌面]# ls
+hello.c
+[root@localhost 桌面]# cd
+[root@localhost ~]# ls
+anaconda - ks.cfg       install.log.syslog                   模板  下载
+anaconda - screenshots  VMwareTools - 10.3.10 - 13959562.tar.gz  视频  音乐
+b                     vmware - tools - distrib                 图片  桌面
+install.log           公共的                               文档
+[root@localhost ~]# ls - l
+总用量 55224
+- rw------ - . 1 root root     1614 4月   7 03:48 anaconda - ks.cfg
+drwxr - x-- - . 2 root root     4096 4月   7 04 : 27 anaconda - screenshots
+drwxr - xr - x. 3 root root     4096 4月  10 01 : 24 b
+- rw - r--r--. 1 root root    46478 4月   7 03 : 48 install.log
+- rw - r--r--. 1 root root    10033 4月   7 03 : 46 install.log.syslog
+- r--r--r--. 1 root root 56431201 4月   7 05 : 00 VMwareTools - 10.3.10 - 13959562.tar.gz
+drwxr - xr - x. 9 root root     4096 6月  13 2019 vmware - tools - distrib
+drwxr - xr - x. 2 root root     4096 4月  10 00 : 13 公共的
+drwxr - xr - x. 2 root root     4096 4月   7 04 : 33 模板
+drwxr - xr - x. 2 root root     4096 4月   7 04 : 33 视频
+drwxr - xr - x. 2 root root     4096 4月   7 04 : 33 图片
+drwxr - xr - x. 2 root root     4096 4月   7 04 : 33 文档
+drwxr - xr - x. 2 root root     4096 4月   7 04 : 33 下载
+drwxr - xr - x. 2 root root     4096 4月   7 04 : 33 音乐
+drwxr - xr - x. 2 root root     4096 4月  14 15 : 00 桌面
+[root@localhost ~]# ls - a
+..gconfd.ssh
+...gnome2.tcshrc
+.abrt.gnote.thumbnails
+anaconda - ks.cfg.gnupg.viminfo
+anaconda - screenshots.gstreamer - 0.10     VMwareTools - 10.3.10 - 13959562.tar.gz
+b.gtk - bookmarks      vmware - tools - distrib
+.bash_history.gvfs               公共的
+.bash_logout.ICEauthority       模板
+.bash_profile.imsettings.log     视频
+.bashrc               install.log         图片
+.cache                install.log.syslog  文档
+.config.lesshst            下载
+.cshrc.local              音乐
+.dbus.nautilus           桌面
+.esd_auth.pulse
+[root@localhost 桌面]# vim / etc / vimrc
+[root@localhost 桌面]# vim hello.c
+[root@localhost 桌面]# vim ~/ .viminfo
+[root@localhost 桌面]# 
