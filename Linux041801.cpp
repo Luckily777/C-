8af9df103c68f3d3ba7b9b54554ed858d8aae747
luckily777 <253974227@qq.com>
2020-04-18 21:15:10 +0800

    软件安装前奏步骤

---
+++ Linux041801.cpp
@@ -0,0 +1,232 @@
+软件安装
+xorg - x11 - drv - ast - 1.1.5 - 1.el6.x86_64.rpm
+xorg - x11 - drv - ati - 7.6.1 - 2.el6.x86_64.rpm
+xorg - x11 - drv - ati - firmware - 7.6.1 - 2.el6.noarch.rpm
+xorg - x11 - drv - cirrus - 1.5.3 - 1.el6.x86_64.rpm
+xorg - x11 - drv - dummy - 0.3.6 - 16.el6.x86_64.rpm
+xorg - x11 - drv - elographics - 1.4.1 - 4.el6.x86_64.rpm
+xorg - x11 - drv - evdev - 2.9.2 - 1.el6.x86_64.rpm
+xorg - x11 - drv - fbdev - 0.4.3 - 17.el6.x86_64.rpm
+xorg - x11 - drv - fpit - 1.4.0 - 7.el6.x86_64.rpm
+xorg - x11 - drv - glint - 1.2.8 - 11.el6.x86_64.rpm
+xorg - x11 - drv - hyperpen - 1.4.1 - 6.el6.x86_64.rpm
+xorg - x11 - drv - i128 - 1.3.6 - 11.el6.x86_64.rpm
+xorg - x11 - drv - i740 - 1.3.4 - 12.el6.x86_64.rpm
+xorg - x11 - drv - intel - 2.99.917 - 0.4.20151111.el6.x86_64.rpm
+xorg - x11 - drv - keyboard - 1.8.1 - 1.el6.x86_64.rpm
+xorg - x11 - drv - mach64 - 6.9.4 - 10.el6.x86_64.rpm
+xorg - x11 - drv - mga - 1.6.3 - 9.el6.x86_64.rpm
+xorg - x11 - drv - mouse - 1.9.1 - 1.el6.x86_64.rpm
+xorg - x11 - drv - mutouch - 1.3.0 - 6.el6.x86_64.rpm
+xorg - x11 - drv - nouveau - 1.0.12 - 1.el6.x86_64.rpm
+xorg - x11 - drv - nv - 2.1.20 - 6.el6.x86_64.rpm
+xorg - x11 - drv - openchrome - 0.3.3 - 7.el6.x86_64.rpm
+xorg - x11 - drv - penmount - 1.5.0 - 6.el6.x86_64.rpm
+xorg - x11 - drv - qxl - 0.1.1 - 19.el6.x86_64.rpm
+xorg - x11 - drv - r128 - 6.10.0 - 1.el6.x86_64.rpm
+xorg - x11 - drv - rendition - 4.2.6 - 1.el6.x86_64.rpm
+xorg - x11 - drv - s3virge - 1.10.7 - 1.el6.x86_64.rpm
+xorg - x11 - drv - savage - 2.3.7 - 3.el6.x86_64.rpm
+xorg - x11 - drv - siliconmotion - 1.7.8 - 1.el6.x86_64.rpm
+xorg - x11 - drv - sis - 0.10.8 - 1.el6.x86_64.rpm
+xorg - x11 - drv - sisusb - 0.9.6 - 11.el6.x86_64.rpm
+xorg - x11 - drv - synaptics - 1.8.3 - 1.el6.x86_64.rpm
+xorg - x11 - drv - tdfx - 1.4.6 - 1.el6.x86_64.rpm
+xorg - x11 - drv - trident - 1.3.6 - 11.el6.x86_64.rpm
+xorg - x11 - drv - v4l - 0.2.0 - 37.el6.x86_64.rpm
+xorg - x11 - drv - vesa - 2.3.4 - 1.el6.x86_64.rpm
+xorg - x11 - drv - vmmouse - 13.1.0 - 1.el6.x86_64.rpm
+xorg - x11 - drv - vmware - 13.1.0 - 2.el6.x86_64.rpm
+xorg - x11 - drv - void - 1.4.1 - 1.el6.x86_64.rpm
+xorg - x11 - drv - voodoo - 1.2.5 - 11.el6.x86_64.rpm
+xorg - x11 - drv - wacom - 0.32.0 - 1.el6.x86_64.rpm
+xorg - x11 - drv - xgi - 1.6.1 - 1.el6.x86_64.rpm
+xorg - x11 - fonts - 100dpi - 7.2 - 11.el6.noarch.rpm
+xorg - x11 - fonts - 75dpi - 7.2 - 11.el6.noarch.rpm
+xorg - x11 - fonts - cyrillic - 7.2 - 11.el6.noarch.rpm
+xorg - x11 - fonts - ethiopic - 7.2 - 11.el6.noarch.rpm
+xorg - x11 - fonts - ISO8859 - 1 - 100dpi - 7.2 - 11.el6.noarch.rpm
+xorg - x11 - fonts - ISO8859 - 14 - 100dpi - 7.2 - 11.el6.noarch.rpm
+xorg - x11 - fonts - ISO8859 - 14 - 75dpi - 7.2 - 11.el6.noarch.rpm
+xorg - x11 - fonts - ISO8859 - 15 - 75dpi - 7.2 - 11.el6.noarch.rpm
+xorg - x11 - fonts - ISO8859 - 1 - 75dpi - 7.2 - 11.el6.noarch.rpm
+xorg - x11 - fonts - ISO8859 - 2 - 100dpi - 7.2 - 11.el6.noarch.rpm
+xorg - x11 - fonts - ISO8859 - 2 - 75dpi - 7.2 - 11.el6.noarch.rpm
+xorg - x11 - fonts - ISO8859 - 9 - 100dpi - 7.2 - 11.el6.noarch.rpm
+xorg - x11 - fonts - ISO8859 - 9 - 75dpi - 7.2 - 11.el6.noarch.rpm
+xorg - x11 - fonts - misc - 7.2 - 11.el6.noarch.rpm
+xorg - x11 - fonts - Type1 - 7.2 - 11.el6.noarch.rpm
+xorg - x11 - font - utils - 7.2 - 11.el6.x86_64.rpm
+xorg - x11 - proto - devel - 7.7 - 13.el6.noarch.rpm
+xorg - x11 - server - common - 1.17.4 - 9.el6.centos.x86_64.rpm
+xorg - x11 - server - utils - 7.7 - 14.el6.x86_64.rpm
+xorg - x11 - server - Xephyr - 1.17.4 - 9.el6.centos.x86_64.rpm
+xorg - x11 - server - Xorg - 1.17.4 - 9.el6.centos.x86_64.rpm
+xorg - x11 - twm - 1.0.3 - 5.1.el6.x86_64.rpm
+xorg - x11 - utils - 7.5 - 14.el6.x86_64.rpm
+xorg - x11 - xauth - 1.0.9 - 1.el6.x86_64.rpm
+xorg - x11 - xdm - 1.1.6 - 14.1.el6.x86_64.rpm
+xorg - x11 - xinit - 1.0.9 - 14.el6.x86_64.rpm
+xorg - x11 - xkb - utils - 7.7 - 12.el6.x86_64.rpm
+xrestop - 0.4 - 7.1.el6.x86_64.rpm
+xsane - 0.997 - 8.el6.x86_64.rpm
+xsane - common - 0.997 - 8.el6.x86_64.rpm
+xsane - gimp - 0.997 - 8.el6.x86_64.rpm
+xsettings - kde - 0.11 - 1.el6.x86_64.rpm
+xterm - 253 - 1.el6.x86_64.rpm
+xulrunner - 17.0.10 - 1.el6.centos.x86_64.rpm
+xvattr - 1.3 - 18.el6.x86_64.rpm
+xz - 4.999.9 - 0.5.beta.20091007git.el6.x86_64.rpm
+xz - devel - 4.999.9 - 0.5.beta.20091007git.el6.x86_64.rpm
+xz - libs - 4.999.9 - 0.5.beta.20091007git.el6.x86_64.rpm
+xz - lzma - compat - 4.999.9 - 0.5.beta.20091007git.el6.x86_64.rpm
+yajl - 1.0.7 - 3.el6.x86_64.rpm
+yelp - 2.28.1 - 17.el6_3.x86_64.rpm
+ypbind - 1.20.4 - 33.el6.x86_64.rpm
+ypserv - 2.19 - 31.el6.x86_64.rpm
+yp - tools - 2.9 - 12.el6.x86_64.rpm
+yum - 3.2.29 - 73.el6.centos.noarch.rpm
+yum - metadata - parser - 1.1.2 - 16.el6.x86_64.rpm
+yum - plugin - aliases - 1.1.30 - 37.el6.noarch.rpm
+yum - plugin - changelog - 1.1.30 - 37.el6.noarch.rpm
+yum - plugin - fastestmirror - 1.1.30 - 37.el6.noarch.rpm
+yum - plugin - security - 1.1.30 - 37.el6.noarch.rpm
+yum - plugin - tmprepo - 1.1.30 - 37.el6.noarch.rpm
+yum - plugin - verify - 1.1.30 - 37.el6.noarch.rpm
+yum - plugin - versionlock - 1.1.30 - 37.el6.noarch.rpm
+yum - presto - 0.6.2 - 1.el6.noarch.rpm
+yum - utils - 1.1.30 - 37.el6.noarch.rpm
+zd1211 - firmware - 1.4 - 4.el6.noarch.rpm
+zenity - 2.28.0 - 1.el6.x86_64.rpm
+zip - 3.0 - 1.el6_7.1.x86_64.rpm
+zlib - 1.2.3 - 29.el6.x86_64.rpm
+zlib - devel - 1.2.3 - 29.el6.x86_64.rpm
+zsh - 4.3.11 - 4.el6.centos.2.x86_64.rpm
+[root@localhost Packages]# ls vim *
+vim - common - 7.4.629 - 5.el6.x86_64.rpm
+vim - enhanced - 7.4.629 - 5.el6.x86_64.rpm
+vim - filesystem - 7.4.629 - 5.el6.x86_64.rpm
+vim - minimal - 7.4.629 - 5.el6.x86_64.rpm
+vim - X11 - 7.4.629 - 5.el6.x86_64.rpm
+[root@localhost Packages]# cp vim -
+vim - common - 7.4.629 - 5.el6.x86_64.rpm
+vim - enhanced - 7.4.629 - 5.el6.x86_64.rpm
+vim - filesystem - 7.4.629 - 5.el6.x86_64.rpm
+vim - minimal - 7.4.629 - 5.el6.x86_64.rpm
+vim - X11 - 7.4.629 - 5.el6.x86_64.rpm
+[root@localhost Packages]# cp vim -
+vim - common - 7.4.629 - 5.el6.x86_64.rpm
+vim - enhanced - 7.4.629 - 5.el6.x86_64.rpm
+vim - filesystem - 7.4.629 - 5.el6.x86_64.rpm
+vim - minimal - 7.4.629 - 5.el6.x86_64.rpm
+vim - X11 - 7.4.629 - 5.el6.x86_64.rpm
+[root@localhost Packages]# cp vim - common - 7.4.629 - 5.el6.x86_64.rpm  ~/
+You have new mail in / var / spool / mail / root
+[root@localhost Packages]# cp vim - enhanced - 7.4.629 - 5.el6.x86_64.rpm ~/
+[root@localhost Packages]# cd
+[root@localhost ~]# ls
+87v
+anaconda - ks.cfg
+anaconda - screenshots
+a.out
+b
+install.log
+install.log.syslog
+my.p
+test.c
+vim - common - 7.4.629 - 5.el6.x86_64.rpm
+vim - enhanced - 7.4.629 - 5.el6.x86_64.rpm
+VMwareTools - 10.3.10 - 13959562.tar.gz
+vmware - tools - distrib
+公共的
+模板
+视频
+图片
+文档
+下载
+音乐
+桌面
+[root@localhost ~]# rpm - i vim - enhanced - 7.4.629 - 5.el6.x86_64.rpm
+warning : vim - enhanced - 7.4.629 - 5.el6.x86_64.rpm : Header V3 RSA / SHA1 Signature, key ID c105b9de : NOKEY
+package vim - enhanced - 2 : 7.4.629 - 5.el6.x86_64 is already installed
+[root@localhost ~]# rpm - i vim - common - 7.4.629 - 5.el6.x86_64.rpm
+warning : vim - common - 7.4.629 - 5.el6.x86_64.rpm : Header V3 RSA / SHA1 Signature, key ID c105b9de : NOKEY
+package vim - common - 2 : 7.4.629 - 5.el6.x86_64 is already installed
+[root@localhost ~]# vim test.c
+[root@localhost ~]# rpm - e vim - enhanced
+[root@localhost ~]# rpm - e vim - common
+warning : / etc / vimrc saved as / etc / vimrc.rpmsave
+[root@localhost ~]# vim tese.c
+bash : / usr / bin / vim : 没有那个文件或目录
+[root@localhost ~]# vim test.c
+bash : / usr / bin / vim : 没有那个文件或目录
+[root@localhost ~]# rpm - i vim - common - 7.4.629 - 5.el6.x86_64.rpm
+warning : vim - common - 7.4.629 - 5.el6.x86_64.rpm : Header V3 RSA / SHA1 Signature, key ID c105b9de : NOKEY
+[root@localhost ~]# rpm - i vim - enhanced - 7.4.629 - 5.el6.x86_64.rpm
+warning : vim - enhanced - 7.4.629 - 5.el6.x86_64.rpm : Header V3 RSA / SHA1 Signature, key ID c105b9de : NOKEY
+[root@localhost ~]# vim test.c
+[root@localhost ~]# vim / etc / yum.
+[root@localhost ~]# vim / etc / yum.
+[root@localhost ~]# ls / tec
+ls : 无法访问 / tec : 没有那个文件或目录
+[root@localhost ~]# cd / etc
+[root@localhost etc]# ls
+[root@localhost etc]# cd
+[root@localhost ~]# cd / etc / yum
+[root@localhost yum]# cd
+[root@localhost ~]# vim / etc / yum
+[root@localhost ~]# cd / etc / yum
+[root@localhost yum]# cd
+[root@localhost ~]# vim / etc / yum.repos.d /
+[root@localhost ~]# vim / etc / yum.repos.d /
+[root@localhost ~]# vim test.c
+[root@localhost ~]# vim / etc / yum.repos.d
+[root@localhost ~]# vim / etc / yum.repos.d / CentOS -
+CentOS - Base.repo       CentOS - Media.repo
+CentOS - Debuginfo.repo  CentOS - Vault.repo
+CentOS - fasttrack.repo
+[root@localhost ~]# vim / etc / yum.repos.d / CentOS -
+CentOS - Base.repo       CentOS - Media.repo
+CentOS - Debuginfo.repo  CentOS - Vault.repo
+CentOS - fasttrack.repo
+[root@localhost ~]# vim / etc / yum.repos.d / CentOS - Base.repo
+[root@localhost ~]# vim / etc / yum.repos.d / CentOS - Base.repo
+87v
+.abrt /
+anaconda - ks.cfg
+anaconda - screenshots /
+a.out
+b /
+.bash_history
+.bash_logout
+.bash_profile
+.bashrc
+.cache /
+.config /
+.cshrc
+.dbus /
+.esd_auth
+.gconf /
+.gconfd /
+.gnome2 /
+.gnote /
+.gnupg /
+.gstreamer - 0.10 /
+.gtk - bookmarks
+.gvfs /
+.ICEauthority
+.imsettings.log
+install.log
+install.log.syslog
+.lesshst
+.local /
+my.p
+.nautilus /
+.pulse /
+.pulse - cookie
+.ssh /
+--More-- ^ C
+[root@localhost ~]# vim / etc / yum.repos.d / CentOS - Base.repo.backup
+[root@localhost ~]# vim / etc / yum.repos.d//CentOS-Base.repo .backup
+还有 2 个文件等待编辑
+[root@localhost ~]# 
