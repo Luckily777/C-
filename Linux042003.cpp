796947207de94f4fdaacd09557b7b314afe95ecb
luckily777 <253974227@qq.com>
2020-04-20 22:33:04 +0800

    软件安装

---
+++ Linux042003.cpp
@@ -0,0 +1,8 @@
+Èí¼þ°²×°
+
+CentOS_BuildTag  EULA  images    LiveOS    repodata              RPM - GPG - KEY - CentOS - Testing - 7
+EFI              GPL   isolinux  Packages  RPM - GPG - KEY - CentOS - 7  TRANS.TBL
+[root@luckily mnt]# cd Packages /
+[root@luckily Packages]# pwd
+/ mnt / Packages
+[root@luckily Packages]# 
