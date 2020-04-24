a16f42a82d4b4e13d243f9df02e4aa9c8471b48b
luckily777 <253974227@qq.com>
2020-04-24 19:18:01 +0800

    101. 对称二叉树

---
+++ Likou042405.cpp
@@ -0,0 +1,30 @@
+101. 对称二叉树
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+bool isSy(struct TreeNode* p, struct TreeNode* q) {
+    //同时走到空， 结构相同
+    if (q == NULL && p == NULL)
+        return true;
+    //没有同时走到空， 结构不同
+    if (q == NULL || p == NULL)
+        return false;
+    // 对应位置的值以及子结构是否相同
+    return p->val == q->val
+        && isSy(p->left, q->right)
+        && isSy(p->right, q->left);
+}
+
+bool isSymmetric(struct TreeNode* root) {
+    if (root == NULL)
+        return true;
+    return isSy(root->left, root->right);
+}
