738d3179e67e811e53a616714fd9695e58b65bd5
luckily777 <253974227@qq.com>
2020-04-27 17:10:49 +0800

    练习 042704

---
+++ Data042704.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include "StackUp.h"
+#include "BinaryTreeUp.h"
+
+void testPre()
+{
+	char str[] = { "abc##de#g##f###" };
+	int idx = 0;
+	int num = 0;
+	BTNode* root = BinaryTreeCreate(str, &idx);
+	printf("pre :\n");
+	BinaryTreePrevOrder(root);
+	printf("\n");
+	printf("pre NoR: \n");
+	BinaryTreePrevOrderNoR(root);
+
+	printf("inO :\n");
+	BinaryTreeInOrder(root);
+	printf("\n");
+	printf("inO NoR: \n");
+	BinaryTreeInOrderNoR(root);
+
+	printf("post :\n");
+	BinaryTreePostOrder(root);
+	printf("\n");
+	printf("post NoR: \n");
+	BinaryTreePostOrderNoR(root);
+
+	printf("lever : ");
+	BinaryTreeLevelOrder(root);
+
+	int ret = BinaryTreeComplete(root);
+
+}
+
+void test2()
+{
+	char str[] = { "ABD##E##CF##G##" };
+	int idx = 0;
+	int num = 0;
+	BTNode* root = BinaryTreeCreate(str, &idx);
+	int ret = BinaryTreeComplete(root);
+}
+
+int main()
+{
+	//testPre();
+	test2();
+	return 0;
+}
