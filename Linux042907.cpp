62eb91063af0cbbdee56ba48a971d38ad2e91340
luckily777 <253974227@qq.com>
2020-04-29 17:37:21 +0800

    Linux 环境安装中文输入法

---
+++ Linux042907.cpp
@@ -0,0 +1,11 @@
+Linux 环境安装中文输入法
+
+
+[root@luckily ~]# yum install  ibus - libpinyin
+已加载插件：fastestmirror, langpacks
+Loading mirror speeds from cached hostfile
+* base: mirrors.aliyun.com
+* extras : mirrors.aliyun.com
+* updates : mirrors.aliyun.com
+软件包 ibus - libpinyin - 1.6.91 - 4.el7.x86_64 已安装并且是最新版本
+无须任何处理
