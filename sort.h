573b53f33b417e4b5705cad1c45a50214f057864
luckily777 <253974227@qq.com>
2020-05-04 23:42:28 +0800

    是

---
+++ sort.h
@@ -0,0 +1,8 @@
+#pragma once
+
+void Swap(int* array, int i, int j);
+
+void insertSort(int* array, int n);
+void shellSort(int* array, int n);
+void selectSort(int* array, int n);
+void selectSort2(int* array, int n);
