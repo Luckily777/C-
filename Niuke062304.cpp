e02cca79fd73a2c9596a8b4f14f6b3c65c729449
luckily777 <253974227@qq.com>
2020-06-24 18:11:23 +0800

    计算日期到天数转换

---
+++ Niuke062304.cpp
@@ -0,0 +1,18 @@
+//计算日期到天数转换
+
+#include <iostream>
+using namespace std;
+static int days[13] = { 0,31,59,90,120,151,181,212,243,273,304,334,365 };
+
+int main() {
+    int year, month, day;
+    int nums = 0;
+    while (cin >> year >> month >> day) {
+        nums = days[month - 1] + day;
+        if (month > 2 && (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))) {
+            nums += 1;
+        }
+        cout << nums << endl;
+    }
+    return 0;
+}
