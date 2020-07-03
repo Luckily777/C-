88d13119f0c6149e3693cdfc78617966450acc65
luckily777 <253974227@qq.com>
2020-07-03 20:04:28 +0800

    每日一练第二天01

--- Everyday0201.cpp
+++ Everyday0201.cpp
@@ -1,28 +1,30 @@
-//#include <iostream>
-//using namespace std;
-//
-//int main() {
-//    int n;
-//    cin >> n;
-//    int* nums = new int[n];
-//    for (int i = 0; i < n; i++) {
-//        cin >> nums[i];
-//    }
-//    int size = 1;
-//    for (int i = 0; i < n - 1; i++) {
-//        int flag = 0;
-//        if ((nums[i] < nums[i + 1]) && (nums[i + 1] > nums[i + 2])) {
-//            size++;
-//            flag = 1;
-//        }
-//        if ((nums[i] > nums[i + 1]) && (nums[i + 1] < nums[i + 2])) {
-//            size++;
-//            flag = 1;
-//        }
-//        if (flag == 1) {
-//            i++;
-//        }
-//    }
-//    cout << size << endl;
-//    return 0;
-//}
+//ÅÅĞò×ÓĞòÁĞ
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int* nums = new int[n];
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+    int size = 1;
+    for (int i = 0; i < n - 2; i++) {
+        int flag = 0;
+        if ((nums[i] < nums[i + 1]) && (nums[i + 1] >= nums[i + 2])) {
+            size++;
+            flag = 1;
+        }
+        if ((nums[i] > nums[i + 1]) && (nums[i + 1] <= nums[i + 2])) {
+            size++;
+            flag = 1;
+        }
+        if (flag == 1) {
+            i++;
+        }
+    }
+    cout << size << endl;
+    return 0;
+}
