24b7628aff1b1ced66c3e3a8999c0574bb07b971
luckily777 <253974227@qq.com>
2020-07-07 18:05:31 +0800

    斐波那契数列

---
+++ Everyday0701.cpp
@@ -0,0 +1,30 @@
+//ì³²¨ÄÇÆõÊýÁÐ
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    int f0 = 0, f1 = 1, f;
+    int k = 0, l = 0;
+    while (1) {
+        f = f0 + f1;
+        k = N - f;
+        if (k>=0) {
+            l = k;
+        };
+        if (k < 0) {
+            if (l < (-k)) {
+                cout << l << endl;
+                break;
+            }
+            else {
+                k = -k;
+                cout <<k << endl;
+                break;
+            }
+        }
+        f0 = f1;
+        f1 = f;
+    }
+}
