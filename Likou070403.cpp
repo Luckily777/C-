6a06c505dc3524aac4b4d8234a6b790b72d4a28d
luckily777 <253974227@qq.com>
2020-07-10 21:34:51 +0800

    //107. 二叉树的层次遍历 II

---
+++ Likou070403.cpp
@@ -0,0 +1,41 @@
+//107. 二叉树的层次遍历 II
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        queue<TreeNode*> q;
+        vector<vector<int>> ret;
+        if (root == nullptr) {
+            return ret;
+        }
+        q.push(root);
+        while (!q.empty()) {
+            //队列中现存的元素个数
+            int sz = q.size();
+            //一次获取同一行元素
+            vector<int> row;
+            while (sz-- > 0) {
+                TreeNode* cur = q.front();
+                q.pop();
+                row.push_back(cur->val);
+                if (cur->left)
+                    q.push(cur->left);
+                if (cur->right)
+                    q.push(cur->right);
+            }
+            ret.push_back(row);
+        }
+        //逆置
+        reverse(ret.begin(), ret.end());
+        return ret;
+    }
+};
