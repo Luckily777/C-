560659425cafcecf4576ef6052d7cbf752406801
luckily777 <253974227@qq.com>
2020-07-14 19:11:18 +0800

    //参数解析

---
+++ Everyday1301.cpp
@@ -0,0 +1,31 @@
+//²ÎÊı½âÎö
+
+
+//#include <iostream>
+//#include <string>
+//#include <vector>
+//using namespace std;
+//
+//int main() {
+//    string str;
+//    string str2;
+//    vector<string> v;
+//    while (getline(cin, str)) {
+//        for (int i = 0; i < str.size(); i++) {
+//            if (str[i] == ' ') {
+//                v.push_back(str2);
+//                str2.clear();
+//            }
+//            else {
+//                str2.push_back(str[i]);
+//            }
+//        }
+//        v.push_back(str2);
+//        int count = v.size();
+//        cout << count << endl;
+//        for (int i = 0; i < count; i++) {
+//            cout << v[i] << endl;
+//        }
+//    }
+//    return 0;
+//}
