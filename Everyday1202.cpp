fb347fdf61dbeb885847f40f435543940c1cc71c
luckily777 <253974227@qq.com>
2020-07-14 11:27:32 +0800

    1202

--- Everyday1202.cpp
+++ Everyday1202.cpp
@@ -1,25 +1,52 @@
 //查找组成一个偶数最接近的两个素数
 
-#include<stdio.h>
+//#include<stdio.h>
+//
+//int isprime(int x) {   //判断是否为素数
+//    int flag = 1;
+//    for (int i = 2; i < x; i++) {
+//        if (x % i == 0) {
+//            flag = 0;
+//            break;
+//        }
+//    }
+//    return flag;
+//}
+//
+//int main() {
+//    int n;
+//    while (scanf("%d", &n) != EOF) {
+//        for (int i = n / 2; i >= 2; i--) {    //从中间开始寻找
+//            if (isprime(i) && isprime(n - i)) {
+//                printf("%d\n", i);
+//                printf("%d\n", n - i);
+//                break;
+//            }
+//        }
+//    }
+//    return 0;
+//}
 
-int isprime(int x) {   //判断是否为素数
-    int flag = 1;
-    for (int i = 2; i < x; i++) {
-        if (x % i == 0) {
-            flag = 0;
-            break;
+
+#include <iostream>
+using namespace std;
+int Sunum(int n) {
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0) {
+            return false;
         }
     }
-    return flag;
+    return true;
 }
 
 int main() {
     int n;
-    while (scanf("%d", &n) != EOF) {
-        for (int i = n / 2; i >= 2; i--) {    //从中间开始寻找
-            if (isprime(i) && isprime(n - i)) {
-                printf("%d\n", i);
-                printf("%d\n", n - i);
+    while (cin >> n) {
+        int mid = n / 2;
+        for (int i = mid; i > 2; i--) {
+            if ((Sunum(i)) && (Sunum(n - i))) {
+                cout << i << endl;
+                cout << (n - i) << endl;
                 break;
             }
         }
