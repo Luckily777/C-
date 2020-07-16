471a861b43d85f08d8119b0119593c4e58032c92
luckily777 <253974227@qq.com>
2020-07-16 16:37:20 +0800

    测试strcpy的拷贝

---
+++ LearnCeshi.cpp
@@ -0,0 +1,50 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//
+//
+//int main() {
+//	char c[] = "is you sure?";
+//	strcpy(c, "No");
+//	printf("%s", c);
+//	return 0;
+//}
+
+
+
+//”–Œ Ã‚
+//#include <iostream>
+//using namespace std;
+//
+//int findNumberOf1(int num) {
+//    int count = 0;
+//    while (num != 0) {
+//        count++;
+//        num = num & (num << 1);
+//    }
+//    return count;
+//}
+//
+//int main() {
+//
+//    int n, ans = 0;
+//    while (cin >> n) {
+//        ans = findNumberOf1(n);
+//        cout << ans << endl;
+//    }
+//    return 0;
+//}
+
+//
+//class Solution {
+//public:
+//    int  NumberOf1(int n) {
+//        int count = 0;
+//        while (n != 0) {
+//            count++;
+//            n = n & (n - 1);
+//        }
+//        return count;
+//    }
+//};
