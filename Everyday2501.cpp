5484456bb828a8da4442ead343d5617946971fcb
luckily777 <253974227@qq.com>
2020-07-28 23:14:51 +0800

    星际密码

---
+++ Everyday2501.cpp
@@ -0,0 +1,20 @@
+//–«º √‹¬Î
+
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//int main() {
+//    vector<int> v = { 1, 1 };
+//    for (int i = 2; i < 10001; ++i) {
+//        v.push_back((v[i - 2] + v[i - 1]) % 10000);
+//    }
+//    int n, x;
+//    while (cin >> n) {
+//        for (int i = 0; i < n; ++i) {
+//            cin >> x;
+//            printf("%04d", v[x]);
+//        }
+//        printf("\n");
+//    }
+//    return 0;
+//}
