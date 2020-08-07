07076b79763b62300b10152dc61f5b7e61481de0
luckily777 <253974227@qq.com>
2020-08-07 22:53:51 +0800

    最大子序列和

---
+++ C++0102.cpp
@@ -0,0 +1,17 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    int n;
+//    cin >> n;
+//    int curmax = 0, imax = -999;
+//    int tmp;
+//    for (int i = 0; i < n; i++) {
+//        cin >> tmp;
+//        curmax += tmp;
+//        imax = max(imax, curmax);
+//        curmax = max(curmax, 0);
+//    }
+//    cout << imax << endl;
+//    return 0;
+//}
