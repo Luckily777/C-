e1eb713e89f73658ef1d0012d7f06818f3d4ba7e
luckily777 <253974227@qq.com>
2020-08-07 22:54:18 +0800

    两字符串

---
+++ C++0101.cpp
@@ -0,0 +1,27 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+
+//int main() {
+//    string str1;
+//    string str2;
+//    getline(cin, str1);
+//    getline(cin, str2);
+//    int size1 = str1.size();
+//    int size2 = str2.size();
+//    string str;
+//    for (int i = 0; i < size1; i++) {
+//        int flag = 0;
+//        for (int j = 0; j < size2; j++) {
+//            if (str1[i] == str2[j]) {
+//                flag = 1;
+//            }
+//        }
+//        if (flag == 0) {
+//            str.push_back(str1[i]);
+//        }
+//    }
+//    cout << str << endl;
+//    return 0;
+//}
+
