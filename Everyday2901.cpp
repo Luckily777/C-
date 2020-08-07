0bdbf94df66e0380576ff4a0429e76e87d60e417
luckily777 <253974227@qq.com>
2020-08-07 22:55:07 +0800

    最难的问题

---
+++ Everyday2901.cpp
@@ -0,0 +1,24 @@
+//最难的问题
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main() {
+//    string str;
+//    while (getline(cin, str)) {
+//        int size = str.size();
+//        for (int i = 0; i < size; i++) {
+//            if (str[i] <= 'Z' && str[i] >= 'A') {
+//                if (str[i] <= 'E') {
+//                    str[i] = 'Z' - ('E' - str[i]);
+//                }
+//                else {
+//                    str[i] -= 5;
+//                }
+//            }
+//        }
+//        cout << str << endl;
+//    }
+//    return 0;
+//}
