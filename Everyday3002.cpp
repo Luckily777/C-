3736ecfdf056c58c60c72fc8dddc8ea1029c4edc
luckily777 <253974227@qq.com>
2020-08-08 13:18:05 +0800

    分解因数

---
+++ Everyday3002.cpp
@@ -0,0 +1,26 @@
+//分解因数
+
+//#include<stdio.h>
+//#include<math.h>
+//int main()
+//{
+//    int n, i;
+//    while (scanf("%d", &n) != EOF)
+//    {
+//        printf("%d = ", n);
+//        for (i = 2; i <= sqrt(n); i++)
+//        {
+//            while (n != i)
+//            {
+//                if (n % i == 0)
+//                {
+//                    printf("%d * ", i);
+//                    n = n / i;
+//                }
+//                else break;
+//            }
+//        }
+//        printf("%d\n", n);
+//    }
+//    return 0;
+//}
