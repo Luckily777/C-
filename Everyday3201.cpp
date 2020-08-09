729bad6d20c8656831ed33863bc56c3bf8eba436
luckily777 <253974227@qq.com>
2020-08-09 22:30:23 +0800

    剪画布条

---
+++ Everyday3201.cpp
@@ -0,0 +1,24 @@
+//¼ô»­²¼Ìõ
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main() {
+//    string Flow;
+//    string Smal;
+//    while (cin >> Flow >> Smal) {
+//        size_t pos = 1;
+//        int count = 0;
+//        int size = Smal.size();
+//        while (pos != -1) {
+//            pos = Flow.find(Smal);
+//            if (pos != -1) {
+//                Flow = Flow.substr(pos + size);
+//                count++;
+//            }
+//        }
+//        cout << count << endl;
+//    }
+//    return 0;
+//}
