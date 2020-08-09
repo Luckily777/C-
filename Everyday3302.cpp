103291bfbde11ebf0d1ea6e675696dcc97cb6f69
luckily777 <253974227@qq.com>
2020-08-09 22:31:41 +0800

    养兔子

---
+++ Everyday3302.cpp
@@ -0,0 +1,19 @@
+//ั๘อรืำ
+
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//int main() {
+//    int n;
+//    vector<long> v(91, 0);
+//    v[1] = 1;
+//    v[2] = 2;
+//    for (int i = 3; i < 91; i++) {
+//        v[i] = v[i - 1] + v[i - 2];
+//    }
+//    while (cin >> n) {
+//        cout << v[n] << endl;
+//    }
+//    return 0;
+//}
