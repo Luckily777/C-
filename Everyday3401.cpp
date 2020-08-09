98ae66d52fd9267bc79397b4426afb6a632c5e74
luckily777 <253974227@qq.com>
2020-08-09 22:32:00 +0800

    年会抽奖

---
+++ Everyday3401.cpp
@@ -0,0 +1,24 @@
+//Äê»á³é½±
+
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    int n;
+//    while (cin >> n) {
+//        float count = 0;
+//        for (int i = 2; i <= n; i++) {
+//            count += i/ n;
+//        }
+//        int sums = 0;
+//        sums=(count / n) * 100000;
+//        int Zsum = sums / 1000;
+//        sums = sums % 1000;
+//        int Xsum = 0;
+//        if ((sums % 10) > 4) {
+//            Xsum = (sums / 10) + 1;
+//        }
+//        cout << Zsum << '.' << Xsum << '%' << endl;
+//    }
+//    return 0;
+//}
