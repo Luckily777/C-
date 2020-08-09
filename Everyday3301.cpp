5f5305e56c396141cd8f807f7fc98820310ab359
luckily777 <253974227@qq.com>
2020-08-09 22:31:20 +0800

    收件人列表

---
+++ Everyday3301.cpp
@@ -0,0 +1,25 @@
+//收件人列表
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main() {
+//    int n;
+//    while (cin >> n) {
+//        string str;
+//        getchar();
+//        for (int i = 0; i < n; i++) {
+//            getline(cin, str);
+//            if ((str.find(',') != -1) || (str.find(' ') != -1)) {
+//                cout << '"' << str << '"';
+//            }
+//            else {
+//                cout << str;
+//            }
+//            (i + 1 != n) ? cout << ", " : cout << endl;
+//
+//        }
+//    }
+//    return 0;
+//}
