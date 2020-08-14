6db7f0ac5dc9ea04bd8a967f9cf4a66d8cd29c69
luckily777 <253974227@qq.com>
2020-08-14 22:38:15 +0800

    数据库

---
+++ Everyday3601.cpp
@@ -0,0 +1,27 @@
+//Êý¾Ý¿â
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main() {
+//    int n;
+//    while (cin >> n) {
+//        int count = 0, max_count = 0;
+//        string id, temp;
+//        while (n--) {
+//            cin >> id >> temp;
+//            if (temp == "connect") {
+//                ++count;
+//            }
+//            if (temp == "disconnect") {
+//                --count;
+//            }
+//            if (count > max_count) {
+//                max_count = count;
+//            }
+//        }
+//        cout << max_count << endl;
+//    }
+//    return 0;
+//}
