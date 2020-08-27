a7ef476423044a254a59c30a31a4d2574821486e
luckily777 <253974227@qq.com>
2020-08-27 23:22:00 +0800

    算法16

---
+++ 算法16.cpp
@@ -0,0 +1,26 @@
+//¶þ²æÊ÷µÄ¾µÏñ
+
+/*
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+    TreeNode(int x) :
+            val(x), left(NULL), right(NULL) {
+    }
+};*/
+//class Solution {
+//public:
+//    void Mirror(TreeNode* pRoot) {
+//        if (pRoot == nullptr) {
+//            return;
+//        }
+//        TreeNode* temp = pRoot->left;
+//        pRoot->left = pRoot->right;
+//        pRoot->right = temp;
+//
+//
+//        Mirror(pRoot->left);
+//        Mirror(pRoot->right);
+//    }
+//};
