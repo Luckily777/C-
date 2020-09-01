20e307ae61082ed6941355407b90c6b0059b2090
luckily777 <253974227@qq.com>
2020-09-01 23:53:39 +0800

    深信服02

---
+++ 深信服02.cpp
@@ -0,0 +1,31 @@
+//ÊÖ»úºÅ²éÑ¯
+
+//#include <iostream>
+//#include <string>
+//#include <map>
+//#include <vector>
+//using namespace std;
+//
+//int main() {
+//    int N, M;
+//    vector<string> v;
+//    while (cin >> N >> M) {
+//        for (int i = 0; i < N; i++) {
+//            string str;
+//            cin >> str;
+//            v.push_back(str);
+//        }
+//        for (int i = 0; i < M; i++) {
+//            string tmp;
+//            cin >> tmp;
+//            int cnt = 0;
+//            for (int j = 0; j < N; j++) {
+//                if (v[j].find(tmp) != -1) {
+//                    cnt++;
+//                }
+//            }
+//            cout << cnt << endl;
+//        }
+//    }
+//    return 0;
+//}
