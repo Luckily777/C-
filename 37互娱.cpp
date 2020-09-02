8e8c80aec961d444ce379f7f62cff0e9763a5e0c
luckily777 <253974227@qq.com>
2020-09-02 23:23:32 +0800

    37 互娱

---
+++ 37互娱.cpp
@@ -0,0 +1,18 @@
+//动态规划 零钱兑换
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> v(amount + 1, amount + 1);
+        v[0] = 0;
+        //钱数为0时的最小硬币数也为0
+        for (int i = 0; i <= amount; i++) {
+            for (int j = 0; j < coins.size(); j++) {
+                if (i >= coins[j]) {
+                    v[i] = min(v[i], v[i - coins[j]] + 1);
+                }
+            }
+        }
+        return v[amount] > amount ? -1 : v[amount];
+    }
+};
