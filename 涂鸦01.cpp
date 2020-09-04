4fd3dccec6285c4edaeb6ccee055569ab9638859
luckily777 <253974227@qq.com>
2020-09-04 23:39:57 +0800

    涂鸦

--- 涂鸦01.cpp
+++ 涂鸦01.cpp
@@ -6,26 +6,25 @@ using namespace std;
 int main() {
 	int size;
 	cin >> size;
-	if (size == 0) {
-		cout << 0 << endl;
+	if (size == 0) {//判断输入数组的大小，若为0则直接返回
 		return 0;
 	}
 	vector<int> v;
-	v.resize(size);
+	v.resize(size);//申请数组并且设置大小
 	for (int i = 0; i < size; i++) {
-		cin >> v[i];
+		cin >> v[i];//将数组内容全部存入申请的空间中
 	}
-	sort(v.begin(), v.end());
-	int index = v.size() - 1;
-	int flag = v.size();
-	while (v[index] >= 0&&index>=1) { 
-		--index;
-		flag = index;
+	sort(v.begin(), v.end());//按从小到大的顺序排序
+	int index = v.size() - 1;//指向最后一个元素
+	int flag = v.size();//指向最后的下一个位置，此时为空
+	while (v[index] >= 0&&index>=1) { //如果此位置的值大于零，且不属于第1个位置（从0开始）
+		--index;//则往前走一位
+		flag = index;//并用flag记录当前的位置
 	}
-	int sum = 0;
+	int sum = 0;//和
 	int n = 0;
 	for (int i = flag; i < v.size(); i++) {
-		sum+=(v[i]*(++n));
+		sum+=(v[i]*(++n));//累计和
 	}
 	cout << sum << endl;
 	return 0;
