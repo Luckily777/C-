37b4cddecc889b5369485e5127b44aa5d38a56aa
luckily777 <253974227@qq.com>
2020-09-11 01:31:16 +0800

    算法33

---
+++ 算法33.cpp
@@ -0,0 +1,36 @@
+//·­×ªµ¥´ÊË³ĞòÁĞ
+//
+//class Solution {
+//public:
+//    void Reverse(string& str, int start, int end) {
+//        while (start < end) {
+//            char temp = str[start];
+//            str[start] = str[end];
+//            str[end] = temp;
+//            start++;
+//            end--;
+//        }
+//    }
+//
+//    string ReverseSentence(string str) {
+//        if (str.size() == 0) {
+//            return str;
+//        }
+//        int i = 0;
+//        int j = i;
+//        int len = str.size();
+//        while (i < len) {
+//            while (i < len && !isspace(str[i])) {
+//                i++;
+//            }
+//            Reverse(str, j, i - 1);
+//            while (i < len && isspace(str[i])) {
+//                i++;
+//            }
+//            j = i;
+//        }
+//        Reverse(str, j, i - 1);
+//        Reverse(str, 0, i - 1);
+//        return str;
+//    }
+//};
